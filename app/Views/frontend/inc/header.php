cd2cb275fbe24955a53c566d8d4eac78514ba136
htdev1189 <htdev1189@gmail.com>
2026-05-22 10:56:03 +0700

    logo frontend

--- app/Views/frontend/inc/header.php
+++ app/Views/frontend/inc/header.php
@@ -4,7 +4,6 @@
             <a class="navbar-brand order-1 py-0" href="index.html">
 
             <img loading="preload" decoding="async" class="img-fluid dark-logo" src="<?= base_url('images/setting/' . get_setting()->blog_logo) ?>" alt="" />
-                <!-- <img loading="prelaod" decoding="async" class="img-fluid" src="<?= base_url('frontend/images/logo.png') ?>" alt="Reporter Hugo"> -->
             </a>
             <div class="navbar-actions order-3 ml-0 ml-md-4">
                 <button aria-label="navbar toggler" class="navbar-toggler border-0" type="button" data-toggle="collapse"
